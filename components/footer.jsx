4260c1c5a29c43f12e55facb263b941eaa74097d
taekim90 <taehk15@gmail.com>
2022-04-10 15:04:00 -0700

    fixed resume download link

--- components/footer.jsx
+++ components/footer.jsx
@@ -5,8 +5,10 @@ export default function Footer() {
     return (
         <div className='footer'>
             <div className='center'>
-                <button>Download Resume</button>
-                <a href='https://docs.google.com/document/d/1_qkIe_8ku7_a8hvbinQsab8Ouipq9re6jDkVOT2IHGs/edit?usp=sharing' download></a>
+                {/* <button>Download Resume</button> */}
+                <a href='https://docs.google.com/document/d/1_qkIe_8ku7_a8hvbinQsab8Ouipq9re6jDkVOT2IHGs/export?format=pdf' download>
+                    <button>Download Resume</button>
+                </a>
             </div>
 
             <h4 className='center'>© Tae Kim {new Date().getFullYear()}</h4>
