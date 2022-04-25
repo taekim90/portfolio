d64b05c3e5653f2576c41c777838e7311f2b3214
taekim90 <taehk15@gmail.com>
2022-04-25 14:11:12 -0700

    fixed colored pipes link and new tab opens when pictures clicked

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -9,9 +9,9 @@ export default function Portfolio() {
             <div className="flex-wrap">
 
             <div className="div-space">
-                <a href="https://taekim90.github.io/First-Project-Color-Pipes/" target="_blank" rel="noopener noreferrer" className="center underline" >Colored Pipes</a>
-                <Link href="https://taekim90.github.io/First-Project-Color-Pipes/" target="_blank" rel="noopener noreferrer">
-                    <a className='center'>
+                <a href="https://taekim90.github.io/Color-Pipes/" target="_blank" rel="noopener noreferrer" className="center underline" >Colored Pipes</a>
+                <Link href="https://taekim90.github.io/Color-Pipes/">
+                    <a className="center" target="_blank" rel="noopener noreferrer">
                         <Image
                             src="/pipes.png"             
                             alt="pipes"
@@ -24,8 +24,8 @@ export default function Portfolio() {
       
             <div className="div-space">
                 <a href="https://the-travel-buddy-v1.herokuapp.com/" target="_blank" rel="noopener noreferrer" className="center underline">The Travel Buddy</a>
-                <Link href="https://the-travel-buddy-v1.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-                    <a className='center'>
+                <Link href="https://the-travel-buddy-v1.herokuapp.com/">
+                    <a className="center" target="_blank" rel="noopener noreferrer">
                         <Image 
                             src="/airplane.png"             
                             alt="airplane"
@@ -38,8 +38,8 @@ export default function Portfolio() {
             
             <div className="div-space">
                 <a href="https://kat-inventory-management.netlify.app/login" target="_blank" rel="noopener noreferrer" className="center underline">KAT Inventory Management - Group Project</a>
-                <Link href="https://kat-inventory-management.netlify.app/login" target="_blank" rel="noopener noreferrer">
-                    <a className='center'>
+                <Link href="https://kat-inventory-management.netlify.app/login">
+                    <a className="center" target="_blank" rel="noopener noreferrer">
                         <Image 
                             src="/inventory.png"             
                             alt="inventory"
@@ -52,8 +52,8 @@ export default function Portfolio() {
 
             <div className="div-space">
                 <a href="https://bloggit-v1.herokuapp.com/" target="_blank" rel="noopener noreferrer" className="center underline">BloggIt</a>
-                <Link href="https://bloggit-v1.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-                    <a className='center'>
+                <Link href="https://bloggit-v1.herokuapp.com/">
+                    <a className="center" target="_blank" rel="noopener noreferrer">
                         <Image 
                             src="/bloggit.png"             
                             alt="blog"
